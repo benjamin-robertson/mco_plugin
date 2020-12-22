0878183b8f31245e52692f0231cffc2bfa4e773b
Benjamin Robertson <benrobertson9876@gmail.com>
2020-12-22 17:28:54 +1100

    Added code to module

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,27 @@
 class mco_plugin (
   String $mco_location = "/opt/puppetlabs/mcollective/plugins/mcollective/agent/",
   Variant[Array, Pattern[/^\w*\.\w*$/, /^\w*\/\w*\.\w*$/]] $mco_files = ['shell.rb','shell.ddl','shell/job.rb'],
-  Optional Variant[Array, Pattern[/^\w*\.\w*$/, /^\w*\/\w*\.\w*$/]] $mco_application = undef,
+  #Variant[Array, Pattern[/^\w*\.\w*$/, /^\w*\/\w*\.\w*$/]] $mco_application = undef,
   ) {
-  
+  # check to see if direcotry need to be created.
+  $mco_files.each | String $files| {
+    if $files =~ /^\w*\/\w*\.\w*$/ {
+      $folder_to_create = split($files, '/')
+      file {"${mco_location}${folder_to_create[0]}":
+        ensure => directory,
+      }
+    }
+  }
+
+  # iterate over the array of files to manage
+  $mco_files.each | String $files| {
+    file {"${mco_location}${files}":
+      ensure => present,
+      mode   => '0644',
+      owner  => 'root',
+      group  => 'root',
+      source => "puppet:///modules/profile/${files}",
+      notify => Service['mcollective'],
+    }
+  }
 }
